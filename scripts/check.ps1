1d321943fa558b4022bdf3e0834c8d528f2ec2d6
Harry van Haaren <harry.vanhaaren@openchip.com>
2026-05-05 16:56:42 +0100

    plugin v6: fix windows CI by adding to v6 to mut ex feature list
    Signed-off-by: Harry van Haaren <harry.vanhaaren@openchip.com>

--- scripts/check.ps1
+++ scripts/check.ps1
@@ -25,14 +25,14 @@ foreach ($checkPath in $checkPaths) {
     $manifestPath = Join-Path $checkPath 'Cargo.toml'
 
     & cargo +nightly hack --manifest-path $manifestPath `
-        --mutually-exclusive-features=plugin-api-v0,plugin-api-v1,plugin-api-v2,plugin-api-v3,plugin-api-v4,plugin-api-v5 `
+        --mutually-exclusive-features=plugin-api-v0,plugin-api-v1,plugin-api-v2,plugin-api-v3,plugin-api-v4,plugin-api-v5,plugin-api-v6 `
         --at-least-one-of=plugin-api-v0,plugin-api-v1,plugin-api-v2,plugin-api-v3,plugin-api-v4,plugin-api-v5 `
         --feature-powerset `
         --exclude-no-default-features `
         check --lib -vv
 
     & cargo +nightly hack --manifest-path $manifestPath `
-        --mutually-exclusive-features=plugin-api-v0,plugin-api-v1,plugin-api-v2,plugin-api-v3,plugin-api-v4,plugin-api-v5 `
+        --mutually-exclusive-features=plugin-api-v0,plugin-api-v1,plugin-api-v2,plugin-api-v3,plugin-api-v4,plugin-api-v5,plugin-api-v6 `
         --at-least-one-of=plugin-api-v0,plugin-api-v1,plugin-api-v2,plugin-api-v3,plugin-api-v4,plugin-api-v5 `
         --feature-powerset `
         --exclude-no-default-features `
